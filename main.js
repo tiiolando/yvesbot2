064490eb55fa791bc27380cfb8fa61fc9df5bb82
tiiolando <39249132+tiiolando@users.noreply.github.com>
2021-03-07 10:17:55 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+// index.js
+const Discord = require('discord.js'),
+    client = new Discord.Client({
+        fetchAllMembers: true
+    }),
+    config = require('./config.json'),
+    fs = require('fs')
+ 
+client.login(config.token)
+client.commands = new Discord.Collection()
+ 
+fs.readdir('./commands', (err, files) => {
+    if (err) throw err
+    files.forEach(file => {
+        if (!file.endsWith('.js')) return
+        const command = require(`./commands/${file}`)
+        client.commands.set(command.name, command)
+    })
+})
+ 
+client.on('message', message => {
+    if (message.type !== 'DEFAULT' || message.author.bot) return
+ 
+    const args = message.content.trim().split(/ +/g)
+    const commandName = args.shift().toLowerCase()
+    if (!commandName.startsWith(config.prefix)) return
+    const command = client.commands.get(commandName.slice(config.prefix.length))
+    if (!command) return
+    command.run(message, args, client)
+})
+ 
+client.on('guildMemberAdd', member => {
+    member.guild.channels.cache.get(config.greeting.channel).send(`${member} a rejoint la secte. Nous sommes désormais ${member.guild.memberCount} Yviens ! 🎉`)
+    member.roles.add(config.greeting.role)
+})
+ 
+client.on('guildMemberRemove', member => {
+    member.guild.channels.cache.get(config.greeting.channel).send(`${member.user.tag} a quitté la secte... 😢`)
+})
+ 
